1262c5d3d212dcd36f7acda8a4fb1a11ca8d7d2b
johndiddles <johnadepelumi@gmail.com>
2024-11-21 17:39:00 +0100

    fix: add cors

--- src/lib/createServer.ts
+++ src/lib/createServer.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import v1Routes from "../routes";
+import cors from "cors";
 
 export const createServer = () => {
   const app = express();
   app.use(express.json());
+  app.use(cors());
 
   app.use("/v1", v1Routes);
 
